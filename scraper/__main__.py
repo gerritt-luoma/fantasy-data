74efc5b812d2eab72c28bf2892cd1b40688d00d2
gerritt-luoma <gerritt.luoma@gmail.com>
2022-09-01 12:35:52 -0600

    added dedicated main file

--- scraper/__main__.py
+++ scraper/__main__.py
@@ -1,6 +1,4 @@
-import utils.requestUtils as ru
+from main import main
 
 if __name__ == '__main__':
-    requestHelper = ru.RequestUtils()
-    content = requestHelper.getContent('https://www.pro-football-reference.com/years/2021/week_1.htm')
-    
+    main()
---
+++ scraper/main.py
@@ -0,0 +1,7 @@
+import schedule
+
+def main():
+    print('hello world')
+    # while True:
+    #     Eventually this will fire one per week very early Tuesday mornings to scrape the weekly data
+    #     pass
