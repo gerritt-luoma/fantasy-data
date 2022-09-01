f3be136d52a8e078f237c92023e9e635a2a0c78b
gerritt-luoma <gerritt.luoma@gmail.com>
2022-09-01 13:09:43 -0600

    Started adding scraping utils

--- scraper/main.py
+++ scraper/main.py
@@ -1,7 +1,12 @@
 import schedule
+from utils import RequestUtils
+from utils import ScrapingUtils
 
 def main():
     print('hello world')
+    pageContent = RequestUtils.getContent('https://www.pro-football-reference.com/years/2021/week_1.htm')
+    gameLinks = ScrapingUtils.getWeeklyGames(pageContent)
+    print(gameLinks)
     # while True:
     #     Eventually this will fire one per week very early Tuesday mornings to scrape the weekly data
     #     pass
---
+++ scraper/utils/ScrapingUtils.py
@@ -0,0 +1,34 @@
+from msilib.schema import tables
+from bs4 import BeautifulSoup, Comment
+
+def getSoup(content):
+    contentSoup = None
+    try:
+        contentSoup = BeautifulSoup(content, 'html.parser')
+    except:
+        print("There was an error converting content to soup")
+    return contentSoup
+
+def getWeeklyGames(gamesPage):
+    gamesPageSoup = getSoup(gamesPage)
+    gamesLinks = None
+    if gamesPageSoup:
+        gamesDiv = gamesPageSoup.find('div', { 'class' : 'game_summaries' })
+        gamesList = gamesDiv.find_all('table', { 'class': 'teams' })
+        gamesLinks = []
+        for game in gamesList:
+            teamsData = game.find('td', { 'class': 'gamelink'})
+            link = teamsData.find('a')
+            if link.has_attr('href'):
+                gamesLinks.append(link['href'])
+            else:
+                print('Could not extract link')
+    return gamesLinks
+
+def getStatTables(gamePage):
+    gamePageSoup = getSoup(gamePage)
+    statTables = None
+    if gamePageSoup:
+        statTables = gamePageSoup.find_all('div', { 'class': 'table_wrapper'})
+        statTables = [table['id'] for table in statTables]
+    return statTables
