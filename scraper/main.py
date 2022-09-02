4a709145dbf787e89252a39b282b03fee69896dd
gerritt-luoma <gerritt.luoma@gmail.com>
2022-09-02 06:46:26 +0000

    I can now scrape a full weeks worth of data!

--- scraper/main.py
+++ scraper/main.py
@@ -1,21 +1,5 @@
 import schedule
-from utils import RequestUtils
-from utils import ScrapingUtils
+import WeeklyScraper
 
 def main():
-    # print('hello world')
-    # pageContent = RequestUtils.getContent('https://www.pro-football-reference.com/years/2021/week_1.htm')
-    # gameLinks = ScrapingUtils.getWeeklyGames(pageContent)
-    # print(gameLinks)
-    # while True:
-    #     Eventually this will fire one per week very early Tuesday mornings to scrape the weekly data
-    #     pass
-    pageContent = RequestUtils.getContent('https://www.pro-football-reference.com/boxscores/202109090tam.htm')
-    statTableDivs = ScrapingUtils.getStatTableDivs(pageContent)
-    allScoring = ScrapingUtils.getTableByName('all_player_offense', statTableDivs)
-    head, body = ScrapingUtils.getTableHeadAndBody(allScoring)
-    offenseList = ScrapingUtils.scrapeBasicOffense(head, body)
-    advancedPassingDiv = ScrapingUtils.getTableByName('all_passing_advanced', statTableDivs)
-    advancedPassingTable = ScrapingUtils.getAdvancedTableFromComment(advancedPassingDiv)
-    head, body = ScrapingUtils.getTableHeadAndBody(advancedPassingTable)
-    offenseList = ScrapingUtils.scrapeAdvancedTable(head, body, 'advanced_passing', offenseList)
+    WeeklyScraper.scrapeStats()
