f7542c6ac8065e1510b4e7ccb793bef500eab93f
gerritt-luoma <gerritt.luoma@gmail.com>
2022-09-03 08:27:04 -0600

    Adding comments

--- scraper/__main__.py
+++ scraper/__main__.py
@@ -1,8 +1,9 @@
 import os, sys
 from dotenv import load_dotenv
-sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
 
+sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
 load_dotenv('../.env')
+
 from main import main
 
 if __name__ == '__main__':
--- scraper/utils/DBUtils.py
+++ scraper/utils/DBUtils.py
@@ -1,5 +1,7 @@
 from pymongo import MongoClient
 import os
 
+
+# Just testing for now.  My container does NOT want to work with mongodb
 def tryConnecting():
     client = MongoClient(f'mongodb://{os.environ.get("MONGODB_USERNAME")}:{os.environ.get("MONGODB_PASSWORD")}@localhost:27017/')
