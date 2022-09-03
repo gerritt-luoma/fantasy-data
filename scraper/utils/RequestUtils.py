669bf9e5b1d5ebd37ec3be9fd0df2a6474be5a1a
gerritt-luoma <gerritt.luoma@gmail.com>
2022-09-03 09:08:26 -0600

    added comments

--- scraper/utils/RequestUtils.py
+++ scraper/utils/RequestUtils.py
@@ -1,11 +1,13 @@
 import requests
 
+# headers to mock an actual request instead of it being a python requests request
 headers = {
     'accept': 'application/json, text/plain, */*',
     'origin': 'https://www.pro-football-reference.com',
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
 }
 
+# get hits an endpoint to generate a request.  none if any form of a failure
 def get(url: str) -> requests.Response:
     retval = None
     try:
@@ -19,6 +21,7 @@ def get(url: str) -> requests.Response:
 
     return retval
 
+# getJSON returns the json of a request.  can probably remove this it wasn't useful at all
 def getJSON(url: str):
     jsonVal = None
     resp = get(url)
@@ -29,6 +32,7 @@ def getJSON(url: str):
             print("Failure getting JSON")
     return jsonVal
 
+# getContent hits the specified url and returns the comment if it was a success.  none if not
 def getContent(url: str):
     contentVal = None
     resp = get(url)
