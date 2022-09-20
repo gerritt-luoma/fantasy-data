edf3675ea17e3d99dc46de7304dfe98fa65e34e7
gerritt-luoma <gerritt.luoma@gmail.com>
2022-09-20 01:04:18 +0000

    Added threading

--- scraper/main.py
+++ scraper/main.py
@@ -1,16 +1,23 @@
+import logging
 import schedule
+import threading
 import time
 import WeeklyScraper
-import logging
 
-schedule.every().thursday.at("01:30").do(WeeklyScraper.scrapeStats)
 
 def startLogging():
     logging.basicConfig(filename='/usr/logs/logs.log', format='%(levelname)s: %(filename)s:%(lineno)d - %(message)s:',encoding='utf-8', level=logging.DEBUG)
 
     # Remove logging debug logs
     logging.getLogger("requests").setLevel(logging.WARNING)
+    logging.debug("Logging has started")
 
+def scrapeWeekInThread():
+    jobThread = threading.Thread(target=WeeklyScraper.scrapeStats)
+    jobThread.start()
+
+
+schedule.every().thursday.at("01:30").do(scrapeWeekInThread)
 def main():
     startLogging()
     # When the container is officially running it will scrape every Tuesday at 1:30 AM
